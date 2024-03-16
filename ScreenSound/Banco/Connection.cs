59307ab3d1481e928e9eb98f13b70df91aec7e16
Wesley Schuab <yinnihisoka@gmail.com>
2024-03-16 13:35:37 -0300

    Criando string de conexão

--- ScreenSound/Banco/Connection.cs
+++ ScreenSound/Banco/Connection.cs
@@ -8,5 +8,8 @@ namespace ScreenSound.Banco
 {
     internal class Connection
     {
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;" +
+            "Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;" +
+            "Multi Subnet Failover=False";
     }
 }

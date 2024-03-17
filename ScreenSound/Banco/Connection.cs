5f3936cc58516e70267967a08d1be9b6ddfe00fa
Wesley Schuab <yinnihisoka@gmail.com>
2024-03-17 14:46:31 -0300

    lendo os dados do banco com um laço while

--- ScreenSound/Banco/Connection.cs
+++ ScreenSound/Banco/Connection.cs
@@ -28,6 +28,14 @@ namespace ScreenSound.Banco
             string sql = "SELECT * FROM Artistas";
 
             SqlCommand command = new SqlCommand (sql, connection);
+
+            using SqlDataReader dataReader = command.ExecuteReader ();
+            while (dataReader.Read ())
+            {
+                string nomeArtista = Convert.ToString(dataReader["Nome"]);
+                string bioArtista = Convert.ToString(dataReader["Bio"]);
+                int idArtista = Convert.ToInt32(dataReader["ID"]);
+            }
         }
     }
 }

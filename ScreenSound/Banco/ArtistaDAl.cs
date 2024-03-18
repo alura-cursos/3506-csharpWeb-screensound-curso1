6b7ced4e0de3a0c926a59f44c4b0e45fb87f9ac9
Wesley Schuab <yinnihisoka@gmail.com>
2024-03-18 00:51:25 -0300

    Refatorando metodo listar

--- ScreenSound/Banco/ArtistaDAl.cs
+++ ScreenSound/Banco/ArtistaDAl.cs
@@ -13,24 +13,9 @@ namespace ScreenSound.Banco
         public IEnumerable<Artista> Listar()
         {
             var lista = new List<Artista>();
-            using var connection = new ScreenSoundContext().ObterConexao();
-            connection.Open();
-
-            string sql = "SELECT * FROM Artistas";
-            SqlCommand command = new SqlCommand(sql, connection);
-            using SqlDataReader dataReader = command.ExecuteReader();
-
-            while (dataReader.Read())
-            {
-                string nomeArtista = Convert.ToString(dataReader["Nome"]);
-                string bioArtista = Convert.ToString(dataReader["Bio"]);
-                int idArtista = Convert.ToInt32(dataReader["Id"]);
-                Artista artista = new(nomeArtista, bioArtista) { Id = idArtista };
-
-                lista.Add(artista);
-            }
-
-            return lista;
+            using var context = new ScreenSoundContext();
+           
+            return context.Artistas.ToList();
         }
       /*  public void Adicionar(Artista artista)
         {

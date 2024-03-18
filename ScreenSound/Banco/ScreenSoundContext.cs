6da2065a056fd2f9c4d152860785d553d3b990c0
Wesley Schuab <yinnihisoka@gmail.com>
2024-03-18 00:02:57 -0300

    Rescrevendo o metodo de conexão com banco de dados

--- ScreenSound/Banco/ScreenSoundContext.cs
+++ ScreenSound/Banco/ScreenSoundContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using ScreenSound.Modelos;
 using System;
 using System.Collections.Generic;
@@ -8,18 +9,16 @@ using System.Threading.Tasks;
 
 namespace ScreenSound.Banco
 {
-    internal class ScreenSoundContext
+    internal class ScreenSoundContext: DbContext
     {
         private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;" +
             "Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;" +
             "Multi Subnet Failover=False";
-
-        public SqlConnection ObterConexao()
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            return new SqlConnection(connectionString);
+            optionsBuilder.UseSqlServer(connectionString);
         }
-
-       
+             
 
     }
 }

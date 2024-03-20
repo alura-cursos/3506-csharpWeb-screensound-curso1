fddcb5d6cb062a444453f4a7a224175375bbae73
Wesley Schuab <yinnihisoka@gmail.com>
2024-03-20 14:48:46 -0300

    Mapear o nome da classe como o nome da tabela através do DbSet

--- ScreenSound/Banco/ScreenSoundContext.cs
+++ ScreenSound/Banco/ScreenSoundContext.cs
@@ -12,6 +12,7 @@ namespace ScreenSound.Banco
     internal class ScreenSoundContext: DbContext
     {
         public DbSet<Artista> Artistas { get; set; }
+        public DbSet<Musica> Musica { get; set; }
 
         private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;" +
             "Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;" +

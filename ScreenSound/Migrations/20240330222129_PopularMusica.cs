898e163944ea072c8201fdd1f90bb84584d378ad
Wesley Schuab <yinnihisoka@gmail.com>
2024-03-30 19:30:00 -0300

    Inserindo dados de musicas e downgrade

--- ScreenSound/Migrations/20240330222129_PopularMusica.cs
+++ ScreenSound/Migrations/20240330222129_PopularMusica.cs
@@ -10,13 +10,19 @@ namespace ScreenSound.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Oceano", 1989 });
 
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Flor de Lis", 1976 });
+
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Samurai", 1982 });
+
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Se", 1992 });
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.Sql("DELETE FROM Artistas");
         }
     }
 }

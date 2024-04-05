1388ad191a162a11b36d3eff36d448850ca6dcb7
Wesley Schuab <yinnihisoka@gmail.com>
2024-04-05 15:37:11 -0300

    adicionando a opção mostrar musica por ano no meu principal

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -10,6 +10,7 @@ opcoes.Add(1, new MenuRegistrarArtista());
 opcoes.Add(2, new MenuRegistrarMusica());
 opcoes.Add(3, new MenuMostrarArtistas());
 opcoes.Add(4, new MenuMostrarMusicas());
+opcoes.Add(5, new MenuMostrarMusicasPorAno());
 opcoes.Add(-1, new MenuSair());
 
 void ExibirLogo()
